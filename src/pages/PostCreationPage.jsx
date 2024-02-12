6350caf5807513c6bc8fc6fe7775a67170ce3cb7
stjepan strmecki <stjepan.strmecki01@gmail.com>
2024-02-12 18:36:39 +0100

    PostCreationPage done :D

--- src/pages/PostCreationPage.jsx
+++ src/pages/PostCreationPage.jsx
@@ -7,13 +7,19 @@ import {
   ref,
   uploadBytes,
   getDownloadURL,
-  storage, // Add storage import
-  addDoc, // Add addDoc import
+  storage,
+  addDoc,
 } from "../firebase";
+import Navbar from "../components/navbar/Navbar";
+import "../pages/PostCreationPage.css";
+import uploadImageEmpty from "../images/uploadImageEmpty.png";
+import uploadImageFull from "../images/uploadImageFull.png";
 
 const PostCreationPage = () => {
   const [title, setTitle] = useState("");
   const [image, setImage] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertStyle, setAlertStyle] = useState();
   const navigate = useNavigate();
 
   const handleImageChange = (e) => {
@@ -21,6 +27,12 @@ const PostCreationPage = () => {
   };
 
   const handlePostCreation = async () => {
+    if (!title || !image) {
+      setAlertStyle(true);
+      setShowAlert(true);
+      return;
+    }
+
     const currentUser = auth.currentUser;
     const storageRef = ref(storage, image.name);
     await uploadBytes(storageRef, image);
@@ -34,21 +46,49 @@ const PostCreationPage = () => {
       downvotes: 0,
       comments: [],
     });
-    navigate("/"); // Navigate to the home page after post creation
+    navigate("/");
+  };
+
+  const handleAlertClose = () => {
+    setAlertStyle(false);
+    // Wait for the slide-out animation to finish before removing the alert from the DOM
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 500); // Adjust the delay to match the animation duration
   };
 
   return (
-    <div>
-      <h2>Create a Post</h2>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input type="file" onChange={handleImageChange} />
-      <button onClick={handlePostCreation}>Create Post</button>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h2>Create a Post</h2>
+        <input
+          type="text"
+          placeholder="Write a title"
+          value={title}
+          maxLength={80}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label className="custom-file-upload">
+          <input type="file" onChange={handleImageChange} />
+          {image ? (
+            <img src={uploadImageFull} alt="Uploaded Image" />
+          ) : (
+            <img src={uploadImageEmpty} alt="No Image" />
+          )}
+          {image ? <span>File Selected</span> : <span>No file chosen</span>}
+        </label>
+        <button onClick={handlePostCreation}>Create Post</button>
+      </div>
+      {showAlert && (
+        <div className={`custom-alert ${alertStyle ? "" : "hide"}`}>
+          <div className="alert-message">
+            Please provide a title and select an image.
+          </div>
+          <button onClick={handleAlertClose}>Close</button>
+        </div>
+      )}
+    </>
   );
 };
 
