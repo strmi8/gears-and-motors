d10f4f218c6d6c80988ddb5941c5b80d1cf700d3
stjepan strmecki <stjepan.strmecki01@gmail.com>
2024-02-14 22:46:11 +0100

    Fix navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { isUserAuthenticated, logoutUser } from "../../firebase";
-import "./Navbar.css";
 import gmLogo from "../../images/gmlogo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [authenticated, setAuthenticated] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [hamburgerClicked, setHamburgerClicked] = useState(false);
+  const [rateCarMenuOpen, setRateCarMenuOpen] = useState(false);
+  const [eventsMenuOpen, setEventsMenuOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 750);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 750);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const checkAuthentication = async () => {
@@ -18,10 +36,68 @@ const Navbar = () => {
     checkAuthentication();
   }, []);
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!isSmallScreen) {
+        if (
+          dropdownRef.current &&
+          !dropdownRef.current.contains(event.target) &&
+          !event.target.classList.contains("menu")
+        ) {
+          setMenuOpen(false);
+          setRateCarMenuOpen(false);
+          setEventsMenuOpen(false);
+        }
+      } else {
+        if (
+          dropdownRef.current &&
+          !dropdownRef.current.contains(event.target) &&
+          !event.target.classList.contains("menu") &&
+          !event.target.closest(".rate-car-container") &&
+          !event.target.closest(".events-container")
+        ) {
+          setMenuOpen(false);
+          setRateCarMenuOpen(false);
+          setEventsMenuOpen(false);
+        }
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isSmallScreen]);
+
   const handleLogout = async () => {
     await logoutUser();
     setAuthenticated(false);
     navigate("/login");
+  };
+
+  const toggleMenu = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (!menuOpen) {
+      setMenuOpen(true);
+      setHamburgerClicked(true);
+      setRateCarMenuOpen(false);
+      setEventsMenuOpen(false);
+    } else {
+      setMenuOpen(false);
+      setHamburgerClicked(false);
+    }
+  };
+
+  const toggleRateCarMenu = () => {
+    setRateCarMenuOpen((prevRateCarMenuOpen) => !prevRateCarMenuOpen);
+    setEventsMenuOpen(false);
+  };
+
+  const toggleEventsMenu = () => {
+    setEventsMenuOpen((prevEventsMenuOpen) => !prevEventsMenuOpen);
+    setRateCarMenuOpen(false);
   };
 
   return (
@@ -31,13 +107,13 @@ const Navbar = () => {
       </Link>
       <div
         className={`menu ${menuOpen ? "open" : ""}`}
-        onClick={() => setMenuOpen(!menuOpen)}
+        onMouseDown={(event) => toggleMenu(event)}
       >
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <ul className={menuOpen ? "open" : ""}>
+      <ul className={menuOpen ? "open" : ""} ref={dropdownRef}>
         <li>
           <Link to="/" className="nav-link">
             Home
@@ -48,21 +124,87 @@ const Navbar = () => {
             Gallery
           </Link>
         </li>
-        <li>
-          <Link to="/posts-view" className="nav-link">
-            Rate My Car
-          </Link>
-        </li>
-        <li>
-          <Link to="/organize-event" className="nav-link">
-            Organize an event
-          </Link>
-        </li>
-        <li>
-          <Link to="/view-events" className="nav-link">
-            View events
-          </Link>
-        </li>
+        {isSmallScreen && hamburgerClicked && (
+          <>
+            <li>
+              <Link to="/create-post" className="nav-link">
+                Create Post
+              </Link>
+            </li>
+            <li>
+              <Link to="/posts-view" className="nav-link">
+                View Posts
+              </Link>
+            </li>
+            <li>
+              <Link to="/view-events" className="nav-link">
+                View events
+              </Link>
+            </li>
+            <li>
+              <Link to="/organize-event" className="nav-link">
+                Organize an event
+              </Link>
+            </li>
+          </>
+        )}
+        {!isSmallScreen && (
+          <>
+            <li className={`dropdown ${rateCarMenuOpen ? "highlight" : ""}`}>
+              <div className="rate-car-container">
+                <span
+                  className={`nav-link ${rateCarMenuOpen ? "highlighted" : ""}`}
+                  onClick={toggleRateCarMenu}
+                >
+                  Rate my car
+                </span>
+                <ul
+                  className={
+                    rateCarMenuOpen ? "dropdown-menu open" : "dropdown-menu"
+                  }
+                >
+                  <li>
+                    <Link to="/create-post" className="nav-link">
+                      Create Post
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/posts-view" className="nav-link">
+                      View Posts
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className={`dropdown ${eventsMenuOpen ? "highlight" : ""}`}>
+              <div className="events-container">
+                <span
+                  className={`nav-link ${eventsMenuOpen ? "highlighted" : ""}`}
+                  onClick={toggleEventsMenu}
+                >
+                  Events
+                </span>
+                <ul
+                  className={
+                    eventsMenuOpen ? "dropdown-menu open" : "dropdown-menu"
+                  }
+                >
+                  <li>
+                    <Link to="/view-events" className="nav-link">
+                      View events
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/organize-event" className="nav-link">
+                      Organize an event
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </>
+        )}
+
         {authenticated ? (
           <li>
             <Link className="nav-link" onClick={handleLogout}>
